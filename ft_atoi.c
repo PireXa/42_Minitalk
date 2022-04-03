224adae3fbb73f09d51673a7ed4e4aa96fee29c7
PireXa <98482270+PireXa@users.noreply.github.com>
2022-04-03 21:21:25 +0100

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+#include"libft.h"
+
+long long	ft_atoi(const char *str)
+{
+	int			i;
+	int			n;
+	long long	result;
+
+	i = 0;
+	n = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		n = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * n);
+}
---
+++ server.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include "libft.h"
+
+int i = 0;
+
+int decoder(char *str)
+{
+	int a;
+	int res;
+
+	a = 7;
+	res = 0;
+	while (a >= 0)
+	{
+		if (str[a] == '1')
+			res += 1 * ft_recursive_power(2, (7 - a));
+		a--;
+	}
+//	printf("%c\n", res);
+	return (res);
+}
+
+void receive_str(int sinal)
+{
+	char	*str;
+	int cena;
+
+	if (sinal == 30)
+	{
+		str[i] = '0';
+		i++;
+	}
+	else if (sinal == 31)
+	{
+		str[i] = '1';
+		i++;
+	}
+	if (i == 8)
+	{
+		str[i] = '\0';
+		cena = decoder(str);
+		printf("%c\n", cena);
+		i = 0;
+	}
+}
+
+int main()
+{
+	printf("\nServer PID: %d\n", getpid());
+	signal(SIGUSR1, receive_str);
+	signal(SIGUSR2, receive_str);
+	while(1);
+}
